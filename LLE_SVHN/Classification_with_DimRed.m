a6fab0f165142b41ca5d5f3979d82eebb7c6df06
Katsileros Petros <katsileros@gmail.com>
2016-02-23 20:38:39 +0200

    fixed minor error

--- LLE_SVHN/Classification_with_DimRed.m
+++ LLE_SVHN/Classification_with_DimRed.m
@@ -2,73 +2,111 @@
 %  \brief     Classification on reduced dimensionality
 %             SVHN dataset with NN algorithm
 %  \author    Katsileros Petros
-%  \date      13/02/2016
+%  \date      19/02/2016
 %  \copyright
-%
+
 
 % Executing nearest neighbor search for classification results
-function err = Classification_with_DimRed(train_descr, test_descr, train_labels, test_labels, N_train, N_test, fid)
+function err = Classification_with_DimRed(train_descr, test_descr, train_labels, test_labels, Ntrain, Ntest, batch_size, fid)
 
 % Make train data
 tmp = train_descr;
 clear train_descr;
-train_descr = zeros(size(tmp{1,1},1),N_train);
+train_descr = zeros(size(tmp{1,1},1),Ntrain);
 
-for i=1:N_train
+for i=1:Ntrain
     train_descr(:,i) = tmp{i,1};
 end
 
 % Make test data
 tmp = test_descr;
 clear test_descr;
-test_descr = zeros(size(tmp{1,1},1),N_test);
+test_descr = zeros(size(tmp{1,1},1),Ntest);
 
-for i=1:N_test
+for i=1:Ntest
     test_descr(:,i) = tmp{i,1};
 end
 clear tmp;
 
-% Nearest neighbors search
-numNeighbors = 8;
-[IDX,~] = knnsearch(train_descr',test_descr','K',numNeighbors);
-% IDX = gpu_knn(train_descr,test_descr,numNeighbors,fid);
-% IDX = IDX(1:numNeighbors,:)';
+all_spaces_classification_labeling = zeros(floor(Ntrain ./ batch_size),(Ntest./(floor(Ntrain ./ batch_size))));
 
-% MNIST labels
-digit_labels = [1:10];
-classification_labeling = zeros(1,N_test);
-error_labels = zeros(1,10);
-overall_digit_labels = zeros(1,10);
+% Iter to all sub-spaces
+for iter=1:floor(Ntrain ./ batch_size)
+    N_train = Ntrain ./ (floor(Ntrain ./ batch_size));
+    N_test = Ntest ./ (floor(Ntrain ./ batch_size));
+    
+    % Nearest neighbors search
+    numNeighbors = 8;
+    [IDX,~] = knnsearch(train_descr(:,(iter-1)*batch_size+1:(iter-1)*batch_size + batch_size)',test_descr(:,(iter-1)*N_test+1:iter*N_test)','K',numNeighbors);
+    IDX = IDX + (iter-1)*batch_size;
+    
+    % MNIST labels 
+    digit_labels = [1:10];
+    classification_labeling = zeros(1,N_test);
+    error_labels = zeros(1,10);
+    overall_digit_labels = zeros(1,10);
 
-for i=1:N_test
+    for i=1:N_test
+        % Digit votings
+        nn_votings = zeros(1,10);
+        for k=1:numNeighbors
+            for j=1:10
+               if(train_labels(IDX(i,k),1) == digit_labels(1,j))
+                    nn_votings(1,j) = nn_votings(1,j) + 1; 
+               end
+            end
+        end
 
-    nn_votings = zeros(1,10);
-    for k=1:numNeighbors
-	for j=1:10
-	  if(train_labels(IDX(i,k),1) == digit_labels(1,j))
-		nn_votings(1,j) = nn_votings(1,j) + 1;
-	  end
-	end
+        [~,classification_labeling(1,i)] = max(nn_votings);
+
+        overall_digit_labels(1,test_labels(i,1)) = overall_digit_labels(1,test_labels(i,1)) + 1;
+
+        % Count accurate results
+        if(classification_labeling(1,i) ~= test_labels(i,1))
+            error_labels(1,test_labels(i,1)) = error_labels(1,test_labels(i,1)) + 1;
+        end
     end
     
-    [~,classification_labeling(1,i)] = max(nn_votings);
-    
-    overall_digit_labels(1,test_labels(i,1)) = overall_digit_labels(1,test_labels(i,1)) + 1;
+    all_spaces_classification_labeling(iter,:) = classification_labeling(1,:); 
 
+    for i=1:10
+        error_labels(1,i) = error_labels(1,i) ./ overall_digit_labels(1,i);
+%         fprintf('Error for digit-%d is: %f \n',mod(i,10),error_labels(1,i).*100);
+    end
+    
+    fprintf(fid,'\nMean average error (subSpace-%d): %f \n', iter, (sum(error_labels) ./ 10 ) .* 100);
+
+end
+
+digit_labels = [1:10];
+subSpace_classification_labeling = zeros(1,N_test);
+error_labels = zeros(1,10);
+% Subspaces voting classification
+for i=1:N_test
+    subSpace_voting = zeros(1,10);
+    for iter=1:floor(N_train ./ batch_size)
+        for j=1:10
+            if(all_spaces_classification_labeling(iter,i) == digit_labels(1,j))
+                subSpace_voting(1,j) = subSpace_voting(1,j) + 1;
+            end
+        end
+    end
+    
+    [~,subSpace_classification_labeling(1,i)] = max(subSpace_voting);
+    
     % Count accurate results
-    if(classification_labeling(1,i) ~= test_labels(i,1))
+    if(subSpace_classification_labeling(1,i) ~= test_labels(i,1))
         error_labels(1,test_labels(i,1)) = error_labels(1,test_labels(i,1)) + 1;
     end
 end
 
 for i=1:10
     error_labels(1,i) = error_labels(1,i) ./ overall_digit_labels(1,i);
-    % fprintf('Error for digit-%d is: %f \n',mod(i,10),error_labels(1,i).*100);
+%     fprintf('Error for digit-%d is: %f \n',mod(i,10),error_labels(1,i).*100);
 end
 
-err = (sum(error_labels) ./ 10 ) .* 100;
-fprintf(fid,'\nMean average error with dimensionality reduction: %f \n', err);
-fprintf('Mean average error with dimensionality reduction: %f \n', err);
+err =  (sum(error_labels) ./ 10 ) .* 100; 
+fprintf(fid,'Mean average error after subSpace voting is: %f \n',err);
+fprintf('Mean average error after subSpace voting is: %f \n\n',err);
 
 end
-
--- LLE_SVHN/SVHN_LLE.m
+++ LLE_SVHN/SVHN_LLE.m
@@ -16,18 +16,15 @@ fprintf(fid,'Start \n %s \n',datestr(now));
 fclose(fid);
 
 % Num of neighbors
-K = [8; 10; 12];
-%K = [8];
+% K = [8; 10; 12];
+K = [12];
 
 % Num of new dimensionality 
-d = [16; 20; 32; 64; 96; 128; 164; 196; 256]; % HoG
-% d = [16; 20; 32; 64; 96]; % SIFT
-% d = [32];
+% d = [16; 20; 32; 64; 96; 128; 164; 196; 256]; % HoG
+d = [32];
 
 % HoG kernel size 
-kernels = {[2 2], [4 4], [8 8]}; % HoG features size {8100,1764,324}
-for kern=1:length(kernels)
-hog_kernel = kernels{1,kern};
+hog_kernel = [4 4];
 folder_prefix = strcat(folder_exp,'kern_size_[',int2str(hog_kernel(1,1)),'x',int2str(hog_kernel(1,2)),']/');
 
 %% Load SVHN dataset
@@ -39,7 +36,7 @@ load('dataset/test_labels.mat');
 
 %% Subsampling data
 % Clust size is the number of train data for each class
-clust_size = 3000;
+clust_size = 200;
 clust_ids = cell(10,1);
 final_data = zeros(size(X,1),size(X,2),clust_size*10);
 final_labels = zeros(clust_size*10,1);
@@ -54,7 +51,12 @@ end
 
 %  Train Data-size
 clear X;
-X = final_data;
+N_train = clust_size*10;
+% Mix train data
+tmp_rand_ids = randperm(N_train);
+X = final_data(:,:,tmp_rand_ids);
+clear train_labels;
+final_labels = final_labels(tmp_rand_ids,1);
 clear final_data;
 N_train = size(X,3);
 
@@ -62,12 +64,9 @@ clear train_labels;
 train_labels = final_labels;
 clear final_labels;
 
-%N_train = 1000;
-% X = single(X(:,:,1:N_train));
-
 % Test Data-size
-N_test = size(testX,3);
-% N_test = 200;
+% N_test = size(testX,3);
+N_test = 100;
 testX = single(testX(:,:,1:N_test));
 
 %% Preprocessing
@@ -82,19 +81,15 @@ train_features = cell(N_train,1);
 train_descriptors = cell(N_train,1);
 train_all_descriptors = [];
 for i=1:N_train
-%     fprintf('Running SIFT-Feature extraction to training image - no: %d \n',i);
-    
-    % SIFT Features Compute
-%     [train_features{i,1},train_descriptors{i,1}] = vl_sift(X(:,:,i)) ;
-%     train_all_descriptors = [train_all_descriptors train_descriptors{i,1}];
+%     fprintf('Running Feature extraction to training image - no: %d \n',i);
     % HoG features
     train_descriptors{i,1} = extractHOGFeatures(X(:,:,i),'CellSize',hog_kernel);
     train_all_descriptors = [train_all_descriptors; train_descriptors{i,1}];
 end
 
-% [unique_train_all_descriptors,unique_train_desc,unique_train_bins] = unique(train_all_descriptors','rows');   % SIFT
 [unique_train_all_descriptors,unique_train_desc,unique_train_bins] = unique(train_all_descriptors,'rows');      % HoG
-unique_train_all_descriptors = double(unique_train_all_descriptors');
+unique_train_all_descriptors = double(unique_train_all_descriptors(unique_train_bins,:)');
+train_all_descriptors = train_all_descriptors(unique_train_bins,:);
 Ntrain = size(unique_train_all_descriptors,2);
 
 fprintf('Test data feature extraction ... \n');
@@ -103,23 +98,21 @@ test_features = cell(N_test,1);
 test_descriptors = cell(N_test,1);
 test_all_descriptors = [];
 for i=1:N_test
-%     fprintf('Running SIFT-Feature extraction to testing image - no: %d \n',i);
-    
-    % SIFT Features Compute
-%     [test_features{i,1},test_descriptors{i,1}] = vl_sift(testX(:,:,i)) ;
-%     test_all_descriptors = [test_all_descriptors test_descriptors{1,1}];
+%     fprintf('Running Feature extraction to testing image - no: %d \n',i);
     % HoG Features
     test_descriptors{i,1} = extractHOGFeatures(testX(:,:,i),'CellSize',hog_kernel);
     test_all_descriptors = [test_all_descriptors; test_descriptors{i,1}];
 end
 
-% [unique_test_all_descriptors,unique_test_desc,unique_test_bins] = unique(test_all_descriptors','rows');       % SIFT
 [unique_test_all_descriptors,unique_test_desc,unique_test_bins] = unique(test_all_descriptors,'rows');          % HoG
-unique_test_all_descriptors = double(unique_test_all_descriptors');
+unique_test_all_descriptors = double(unique_test_all_descriptors(unique_test_bins,:)');
+test_all_descriptors = test_all_descriptors(unique_test_bins,:);
 Ntest = size(unique_test_all_descriptors,2);
 
-% Batch size (Must be grater thna K) 
-batch_size = Ntrain;
+% Batch size 
+batch_size = floor([ Ntrain; (Ntrain./2); (Ntrain./4); (Ntrain./5) ]);
+% batch_size = floor([ Ntrain; (Ntrain./5)]);
+% batch_size = Ntrain;
 
 results_classification_err = zeros(length(K), length(batch_size),length(d));
 
@@ -137,105 +130,91 @@ for i=1:length(K)
         fid=fopen(file_name,'w');
         
         Y = digits(unique_train_all_descriptors, unique_test_all_descriptors, K(i,1), d(end,1), fid, Ntrain, batch_size(k,1), batch_folder_name);
-        tmp_train_all_desc = Y(:,unique_train_bins);
-        tmp_test_all_desc = Y(:,size(unique_train_all_descriptors,2)+unique_test_bins);
-        
+        Ntrain = size(unique_train_all_descriptors,2);
+        Ntest = size(unique_test_all_descriptors,2);
+
         %% Make data-features to the appropriate format
         % Train data
-        final_train_descr = cell(N_train,1);
-%         counter = 0;
-        for t=1:N_train
+        tmp_train_all_desc = [];
+        tmp_test_all_desc = [];
+        for b=1:floor(Ntrain ./ batch_size(k,1))
+            tmp_train_all_desc = [ tmp_train_all_desc Y(:,(b-1)*Ntest + ((b-1)*batch_size(k,1) + 1) : ...
+                                                          (b-1)*Ntest + ((b-1)*batch_size(k,1) + batch_size(k,1)))];
+            tmp_test_all_desc = [tmp_test_all_desc Y(:,(b-1)*(batch_size(k,1)+Ntest)+(batch_size(k,1)+(1:Ntest)))];                                          
+        end
+
+        Ntrain = size(tmp_train_all_desc,2);
+        final_train_descr = cell(Ntrain,1);
+        for t=1:Ntrain
             final_train_descr{t,1} = [];
             final_train_descr{t,1} = [final_train_descr{t,1} tmp_train_all_desc(:,t)] ; % HoG
-%             % SIFT
-%             for l=1:size(train_descriptors{t,1},2)
-%                 final_train_descr{t,1} = [final_train_descr{t,1} tmp_train_all_desc(:,counter+l)] ; % SIFT
-%             end
-%             counter = counter + size(train_descriptors{t,1},2); 
         end
         
         % Test data
+        Ntest = size(tmp_test_all_desc,2);
         final_test_descr = cell(N_test,1);
-%         counter = 0;
-        for t=1:N_test
+        for t=1:Ntest
             final_test_descr{t,1} = [];
             final_test_descr{t,1} = [final_test_descr{t,1} tmp_test_all_desc(:,t)] ; % HoG
-%             % SIFT
-%             for l=1:size(test_descriptors{t,1},2)
-%                 final_test_descr{t,1} = [final_test_descr{t,1} tmp_test_all_desc(:,counter+l)] ; % SIFT
-%             end
-%             counter = counter + size(test_descriptors{t,1},2); 
         end
         
         % Classification results
         tic;
         err = 0;
-        err = Classification_with_DimRed(final_train_descr,final_test_descr, train_labels, test_labels, N_train, N_test, fid) ;
+        err = Classification_with_DimRed(final_train_descr, final_test_descr, train_labels, test_labels, Ntrain, Ntest, batch_size(k,1), fid) ;
 	results_classification_err(i,k,length(d)) = err;
         fprintf(fid,'Classification without dimRed elapsed time: %6f \n',toc);
         fclose(fid);
         
         % Keep the lower dimensions
         for j=1:(length(d)-1)
-            % folder_name = strcat(folder_prefix, int2str(K(i,1)), 'nn_', int2str(d(j,1)), 'd/');
-            % mkdir(strcat(folder_name));
             err = 0;  
             fprintf('Running for K:%d, d:%d, batch_size:%d \n', K(i,1), d(j,1), batch_size(k,1));
-        
-           %  batch_folder_name = strcat(folder_name, int2str(batch_size(k,1)), '_batch_size/');
-           %  mkdir(batch_folder_name);
             
             file_name = strcat(batch_folder_name, int2str(K(i,1)), 'nn_', int2str(d(j,1)), 'd',int2str(batch_size(k,1)),'_batch.txt');
             fid=fopen(file_name,'w');
             
             lowerY = Y(1:d(j,1),:);
             
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%%             ERROR HERE              %%%%
             tmp_train_all_desc = lowerY(:,unique_train_bins);
             tmp_test_all_desc = lowerY(:,size(unique_train_all_descriptors,2)+unique_test_bins);
-        
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
             %% Make data to the appropriate format
             % Train data
             final_train_descr = cell(N_train,1);
-%             counter = 0;
             for t=1:N_train
                 final_train_descr{t,1} = [];
                 final_train_descr{t,1} = [final_train_descr{t,1} tmp_train_all_desc(:,t)] ; % HoG
-%                 % SIFT
-%                 for l=1:size(train_descriptors{t,1},2)
-%                     final_train_descr{t,1} = [final_train_descr{t,1} tmp_train_all_desc(:,counter+l)] ; % SIFT
-%                 end
-%                 counter = counter + size(train_descriptors{t,1},2);
             end
             
             % Test data
             final_test_descr = cell(N_test,1);
-%             counter = 0;
             for t=1:N_test
                 final_test_descr{t,1} = [];
                 final_test_descr{t,1} = [final_test_descr{t,1} tmp_test_all_desc(:,t)] ; % HoG
-%                 % SIFT
-%                 for l=1:size(test_descriptors{t,1},2)
-%                     final_test_descr{t,1} = [final_test_descr{t,1} tmp_test_all_desc(:,counter+l)] ; % SIFT
-%                 end
-%                 counter = counter + size(test_descriptors{t,1},2);
             end
             
             % Classification results
             tic;
             err = 0;
-            err = Classification_with_DimRed(final_train_descr,final_test_descr, train_labels, test_labels, N_train, N_test, fid);
+            err = Classification_with_DimRed(final_train_descr, final_test_descr, train_labels, test_labels, N_train, N_test, batch_size(k,1), fid);
+%             err = Classification_with_DimRed_swsto(final_train_descr, final_test_descr, train_labels, test_labels, N_train, N_test, fid);
             results_classification_err(i,k,j) = err;
-            fprintf(fid,'Classification without dimRed elapsed time: %6f \n',toc);
+            fprintf(fid,'Classification with dimRed elapsed time: %6f \n',toc);
             fclose(fid);
             
         end
     end
     fprintf('\n');
     
-    file_name = strcat(folder_name, int2str(K(i,1)), 'nn_784_d', '.txt');
+    file_name = strcat(folder_name, int2str(K(i,1)), 'nn_No_dimRed', '.txt');
     fid=fopen(file_name,'w');
     
     tic;
+    
     err = Classification_without_DimRed(train_descriptors,test_descriptors, train_labels, test_labels, N_train, N_test, fid);
     fprintf(fid,'Classification without reduction elapsed time: %6f \n',toc);
     fclose(fid);
@@ -244,7 +223,6 @@ end
 fprintf('\n\n END \n');
 save(strcat(folder_prefix,'results_classification_err.mat'),'results_classification_err');
 
-end
 
 fid = fopen(strcat(folder_exp,'time2.txt'),'w');
 fprintf(fid,'End \n %s \n',datestr(now));
