0907929d1aeba096c3258682263a011f2a31c0f1
Katsileros <katsileros@gmail.com>
2016-06-04 16:53:50 +0300

    Changes-1

--- Diploma-Thesis-LaTeX/clear_trash_files.sh
+++ Diploma-Thesis-LaTeX/clear_trash_files.sh
@@ -1,2 +1,2 @@
 rm *.aux *.lof *.log *.lot *.toc *.bbl *.bcf *.blg *.out *.xml
-rm NonLinear_Dimensionality_Reduction_KatsilerosPetros.synctex.gz
+rm Locally_Linear_Embeddings_in_Pattern_Recognition.synctex.gz
